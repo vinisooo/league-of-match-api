28b9fdffa5739b8303be49a9ca083b7a31fcfc59
vinisooo <cviniciusliramdev@gmail.com>
2023-05-16 16:44:24 -0300

    fix: adjust user modal and serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class EloChoices(models.TextChoices):
+    FERRO = "Ferro"
+    BRONZE = "Bronze"
+    PRATA = "Prata"
+    OURO = "Ouro"
+    PLATINA = "Platina"
+    DIAMANTE = "Diamante"
+    MESTRE = "Mestre"
+    GRAO_MESTRE = "Grão-Mestre"
+    DESAFIANTE = "Desafiante"
+
+
+class RouteChoices(models.TextChoices):
+    TOPLANE = "Toplane"
+    JUNGLE = "Jungle"
+    MIDLANE = "Midlane"
+    ADC = "Adc"
+    SUPPORT = "Support"
+
+
+class User(AbstractUser):
+    elo = models.TextField(max_length=25, choices=EloChoices.as_choices())
+    route = models.TextField(
+        max_length=10, choices=RouteChoices.as_choices(), null=True, blank=True
+    )
+    main = models.ForeignKey(
+        "characters.Character",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="users",
+    )
+    profile_icon = models.URLField(null=True, blank=True)
+    bio = models.CharField(blank=True, null=True, max_length=255)
+    discord = models.CharField(blank=True, null=True, max_length=50)
+
+
 from rest_framework import serializers
 from .models import User, RouteChoices, EloChoices
 import re
@@ -26,17 +67,14 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ["id"]
         extra_kwargs = {
             "elo": {
-                "choices": EloChoices,
+                "choices": EloChoices.as_choices(),
             },
-            "route": {"choices": RouteChoices},
+            "route": {"choices": RouteChoices.as_choices()},
             "discord": serializers.CharField(
-                validators=[lambda x: re.match(r"^\d{18}$", x)]
+                validators=[lambda x: x is None or re.match(r"^\d{18}$", x)]
             ),
             "password": {"required": True, "write_only": True},
         }
-
-    def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
